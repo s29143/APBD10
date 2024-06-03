c65012c20f586572165c0f93296c617109bda9d6
s29143 <s29143@pjwstk.edu.pl>
2024-06-03 23:19:34 +0200

    moved project

---
+++ APBD10/Context/ApbdContext.cs
@@ -0,0 +1,31 @@
+﻿using APBD10.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APBD10.Context;
+
+public class ApbdContext : DbContext
+{
+    protected ApbdContext()
+    {
+    }
+
+    public ApbdContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Doctor> Doctors { get; set; }
+    public DbSet<Medicament> Medicaments { get; set; }
+    public DbSet<Patient> Patients { get; set; }
+    public DbSet<Prescription> Prescriptions { get; set; }
+    public DbSet<Prescription_Medicament> PrescriptionMedicaments { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Prescription_Medicament>().HasKey(u => new
+        {
+            u.IdMedicament,
+            u.IdPrescription
+        });
+        
+    }
+}
